3c98fa2c733ffc35c10688065ad3230b6f8dc430
suri1980 <suri1980@gmail.com>
2023-03-02 02:07:29 +0800

    Base Template - First Commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+
+const webpack = require("webpack");
+const path = require("path");
+const PACKAGE = require("./package.json");
+const HtmlWPPlugin = require("html-webpack-plugin");
+const CopyWPPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, ".build"),
+    filename: "appBuild.js",
+    chunkFilename: "[name].js",
+  },
+  module: {
+    rules: [
+        {
+            test: /.(js)$/,
+            exclude: [/node_modules/],
+            loader: 'babel-loader',
+            options: { 
+                presets: ['@babel/env','@babel/preset-react'] 
+            },
+        },
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+
+    new webpack.EnvironmentPlugin({
+      VERSION: PACKAGE.version,
+    }),
+
+    // Take Reference of HTML File.
+    new HtmlWPPlugin({
+      inject: true,
+      template: path.resolve(__dirname, "public/index.html"),
+      APP_ROOT_ID: 'responsivetablecomponent',
+      APP_VERSION: PACKAGE.version
+    }),
+  ],
+  devServer: {
+    open: true,
+    historyApiFallback: true,
+    static: {
+      directory: "./public",
+    },
+    hot: true,
+    port: 4000,
+    proxy: {
+      "/api": "http://YOUR_API_URL:9000",
+    },
+  },
+};    
